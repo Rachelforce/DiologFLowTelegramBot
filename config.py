527ae53b4fb27f3cfebba824d5ee640a471475ad
Rachelforce <55845125+Rachelforce@users.noreply.github.com>
2020-01-27 22:12:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    BOT_TOKEN = "1087956361:AAG8ukD9IMNEjFM5WTskkGR3cxuFklRkoeY"
-    DIOLOGFLOW_TOKEN = 'baa1b6cc73d0451384f8fe712bdb2bc6'
+    BOT_TOKEN = "#"
+    DIOLOGFLOW_TOKEN = '#'
